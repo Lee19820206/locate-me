b7d90302102ffb58d137f309ed9e37cc088d43bb
Yorick Chan <yorick@gmail.com>
2011-03-13 15:38:55 +0800

    Initial Commit

---
+++ locate-meTests/locate_meTests.h
@@ -0,0 +1,17 @@
+//
+//  locate_meTests.h
+//  locate-meTests
+//
+//  Created by Yorick Chan on 13/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface locate_meTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ locate-meTests/locate_meTests.m
@@ -0,0 +1,33 @@
+//
+//  locate_meTests.m
+//  locate-meTests
+//
+//  Created by Yorick Chan on 13/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "locate_meTests.h"
+
+
+@implementation locate_meTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in locate-meTests");
+}
+
+@end
