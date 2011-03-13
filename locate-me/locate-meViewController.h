54ab95e259220ab445ba961a4a222e9df1699e4c
Yorick Chan <yorick@gmail.com>
2011-03-13 15:49:18 +0800

    Create locate-me view controller

---
+++ locate-me/locate-meViewController.h
@@ -0,0 +1,16 @@
+//
+//  locate-meViewController.h
+//  locate-me
+//
+//  Created by Yorick Chan on 13/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface locate_meViewController : UIViewController {
+    
+}
+
+@end
