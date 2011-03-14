820089440f2600aca8ce6566c26b92e5e2b5cb39
Yorick Chan <yorick@gmail.com>
2011-03-14 21:12:22 +0800

    finish take photo function

--- locate-me/locate-meViewController.h
+++ locate-me/locate-meViewController.h
@@ -9,14 +9,25 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 
-@interface locate_meViewController : UIViewController <CLLocationManagerDelegate, MKReverseGeocoderDelegate, MKMapViewDelegate, UIAlertViewDelegate> {
+@interface locate_meViewController : UIViewController <CLLocationManagerDelegate, 
+    MKReverseGeocoderDelegate, MKMapViewDelegate, 
+    UIAlertViewDelegate, UIImagePickerControllerDelegate, 
+    UINavigationControllerDelegate> {
     
     MKMapView *mapView;
     UIProgressView *progressBar;
     UILabel *progressLabel;
     UIButton *button;
+    UIImageView *imageView;
+    UIButton *takeButton;
+    MPMoviePlayerController *moviePlayerController;
+    UIImage *image;
+    NSURL *movieURL;
+    NSString *lastChosenMediaType;
+    CGRect imageFrame;
     
 }
 
@@ -25,6 +36,15 @@
 @property (nonatomic, retain) IBOutlet UILabel *progressLabel;
 @property (nonatomic, retain) IBOutlet UIButton *button;
 
+@property (nonatomic, retain) IBOutlet UIImageView *imageView;
+@property (nonatomic, retain) IBOutlet UIButton *takeButton;
+@property (nonatomic, retain) MPMoviePlayerController *moviePlayerController;
+@property (nonatomic, retain) IBOutlet UIImage *image;
+@property (nonatomic, retain) NSURL *movieURL;
+@property (nonatomic, copy) NSString *lastChosenMediaType;
+
 - (IBAction)locateMe;
+- (IBAction)takePicture:(id)sender;
+- (IBAction)selectExistingPictureOrVideo:(id)sender;
 
 @end
--- locate-me/locate-meViewController.m
+++ locate-me/locate-meViewController.m
@@ -8,7 +8,13 @@
 
 #import "locate-meViewController.h"
 #import "MapLocation.h"
+#import <MobileCoreServices/UTCoreTypes.h>
 
+@interface locate_meViewController ()
+static UIImage *shrinkImage(UIImage *original, CGSize size);
+- (void)updateDisplay;
+- (void)getMediaFromSource:(UIImagePickerControllerSourceType)sourceType;
+@end
 
 @implementation locate_meViewController
 
@@ -16,6 +22,14 @@
 @synthesize progressBar;
 @synthesize progressLabel;
 @synthesize button;
+
+@synthesize imageView;
+@synthesize takeButton;
+@synthesize moviePlayerController;
+@synthesize image;
+@synthesize movieURL;
+@synthesize lastChosenMediaType;
+
 
 #pragma mark -
 
@@ -55,6 +69,14 @@
     [progressBar release];
     [progressLabel release];
     [button release];
+    
+    [imageView release];
+    [moviePlayerController release];
+    [takeButton release];
+    [image release];
+    [movieURL release];
+    [lastChosenMediaType release];
+    
     [super dealloc];
 }
 
@@ -70,6 +92,10 @@
 
 - (void)viewDidLoad
 {
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        takeButton.hidden = YES;
+    }
+    imageFrame = imageView.frame;
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
@@ -80,9 +106,17 @@
     self.progressLabel = nil;
     self.progressBar = nil;
     self.button = nil;
+    self.imageView = nil;
+    self.takeButton = nil;
+    self.moviePlayerController = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self updateDisplay];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -220,6 +254,98 @@
                           otherButtonTitles:nil];
     [alert show];
     [alert release];
+}
+
+#pragma mark -
+#pragma mark TakePicture
+
+- (IBAction)takePicture:(id)sender {
+    [self getMediaFromSource:UIImagePickerControllerSourceTypeCamera];
+}
+
+- (IBAction)selectExistingPictureOrVideo:(id)sender {
+    [self getMediaFromSource:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+#pragma mark -
+#pragma mark UIImagePickerController delegate methods
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    self.lastChosenMediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    if ([lastChosenMediaType isEqual:(NSString *)kUTTypeImage]) {
+        UIImage *chosenImage = [info objectForKey:UIImagePickerControllerEditedImage];
+        UIImage *shrunkenImage = shrinkImage(chosenImage, imageFrame.size);
+        self.image = shrunkenImage;
+    } else if ([lastChosenMediaType isEqual:(NSString *)kUTTypeMovie]) {
+        self.movieURL = [info objectForKey:UIImagePickerControllerMediaURL];
+    }
+    [picker dismissModalViewControllerAnimated:YES];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissModalViewControllerAnimated:YES];
+}
+
+#pragma mark -
+static UIImage *shrinkImage(UIImage *original, CGSize size) {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGContextRef context = CGBitmapContextCreate(NULL, size.width * scale, size.height * scale, 8, 0, colorSpace, kCGImageAlphaPremultipliedFirst);
+    CGContextDrawImage(context, 
+                       CGRectMake(0, 0, size.width * scale, size.height * scale), 
+                       original.CGImage);
+    CGImageRef shrunken = CGBitmapContextCreateImage(context);
+    UIImage *final = [UIImage imageWithCGImage:shrunken];
+    
+    CGContextRelease(context);
+    CGImageRelease(shrunken);
+    
+    return final;
+}
+
+- (void)updateDisplay {
+    if ([lastChosenMediaType isEqual:(NSString *)kUTTypeImage]) {
+        imageView.image = image;
+        imageView.hidden = NO;
+        moviePlayerController.view.hidden = YES;
+    } else if ([lastChosenMediaType isEqual:(NSString *)kUTTypeMovie]) {
+        [self.moviePlayerController.view removeFromSuperview];
+        self.moviePlayerController = [[[MPMoviePlayerController alloc]
+                                       initWithContentURL:movieURL] autorelease];
+        moviePlayerController.view.frame = imageFrame;
+        moviePlayerController.view.clipsToBounds = YES;
+        [self.view addSubview:moviePlayerController.view];
+        imageView.hidden = YES;
+    }
+}
+
+- (void)getMediaFromSource:(UIImagePickerControllerSourceType)sourceType {
+    NSArray *mediaTypes = [UIImagePickerController
+                           availableMediaTypesForSourceType:sourceType];
+    if ([UIImagePickerController isSourceTypeAvailable:
+         sourceType] && [mediaTypes count] > 0) {
+        NSArray *mediaTypes = [UIImagePickerController
+                               availableMediaTypesForSourceType:sourceType];
+        UIImagePickerController *picker =
+        [[UIImagePickerController alloc] init];
+        picker.mediaTypes = mediaTypes;
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        picker.sourceType = sourceType;
+        [self presentModalViewController:picker animated:YES];
+        [picker release];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Error accessing media"
+                              message:@"Device doesn’t support that media source."
+                              delegate:nil
+                              cancelButtonTitle:@"Drat!"
+                              otherButtonTitles:nil];
+        [alert show];
+        [alert release];
+    }
 }
 
 @end
